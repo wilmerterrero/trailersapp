5f7e3834f3cd5fb3f433fe92e099928e8bff2cbf
wilmerterrero <wilmerterrerp1922@gmail.com>
2020-12-04 01:02:47 -0400

    solving role bug

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -6,7 +6,6 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Response;
-
 class AdminMiddleware
 {
     /**
@@ -19,7 +18,7 @@ class AdminMiddleware
     public function handle(Request $request, Closure $next)
     {
         $user = Auth::user();
-        if ($user->role == 'admin') {
+        if ($user->hasRole('admin')) {
             return $next($request);
         } else {
             // abort(403, 'Wrong Accept Header');
