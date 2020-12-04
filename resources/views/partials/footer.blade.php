87677f27a3a759f88e24a838dcfc6dcfb1afbc28
wilmerterrero <wilmerterrerp1922@gmail.com>
2020-12-04 01:39:50 -0400

    moving footer to partils views

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,41 @@
+ <footer class="site-footer">
+     <div class="container-footer">
+         <div class="row">
+             <div class="col-sm-12 col-md-6">
+                 <div class="container-footer">
+                     <div class="row">
+                         <div class="col-md-8 col-sm-6 col-xs-12">
+                             <p class="copyright-text">Copyright &copy; 2019-8127 All Rights Reserved by
+                                 <a href="#">Wilmer Terrero & Naomi Céspedes</a>.
+                             </p>
+                         </div>
+                     </div>
+                 </div>
+                 <p class="text-justify"> <br> Todos los contenidos de este Sitio (Incluyendo, pero no limitado a,
+                     texto, logotipos, contenido, fotografías, audio, botones, nombres comerciales y vídeo) están
+                     sujetos a derechos de propiedad por las leyes de Derechos de autor.
+                 </p>
+             </div>
+
+             <div class="col-xs-6 col-md-3">
+                 <ul class="social-icons">
+                     <br><br><br>
+                     <li><a class="facebook" href="#"><i class="fab fa-facebook"></i></a></li>
+                     <li><a class="twitter" href="#"><i class="fab fa-twitter"></i></a></li>
+                     <li><a class="github" href="#"><i class="fab fa-github"></i></a></li>
+                     <li><a class="youtube" href="#"><i class="fab fa-youtube"></i></a></li>
+                 </ul>
+
+             </div>
+
+             <div class="col-xs-6 col-md-3">
+                 <h6>Mas Informacion</h6>
+                 <ul class="footer-links">
+                     <li><a href="mailto:20198127@itla.edu.do">About Us</a></li>
+                     <li><a href="mailto:20198127@itla.edu.do">Contact Us</a></li>
+                 </ul>
+             </div>
+         </div>
+         <hr>
+     </div>
+ </footer>
