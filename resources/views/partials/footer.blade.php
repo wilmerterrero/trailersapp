591be0b35cdb8a1b4bac7e47cec462d5220bcc0e
Naeliza <lizcespedes0326@gmail.com>
2020-12-04 19:40:03 -0400

    Fixing footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,41 +1,70 @@
- <footer class="site-footer">
-     <div class="container-footer">
+ <!-- Footer -->
+ <footer class="page-footer font-small site-footer pt-4">
+
+     <!-- Footer Links -->
+     <div class="container-fluid text-center text-md-left">
+
+         <!-- Grid row -->
          <div class="row">
-             <div class="col-sm-12 col-md-6">
-                 <div class="container-footer">
-                     <div class="row">
-                         <div class="col-md-8 col-sm-6 col-xs-12">
-                             <p class="copyright-text">Copyright &copy; 2019-8127 All Rights Reserved by
-                                 <a href="#">Wilmer Terrero & Naomi Céspedes</a>.
-                             </p>
-                         </div>
-                     </div>
-                 </div>
-                 <p class="text-justify"> <br> Todos los contenidos de este Sitio (Incluyendo, pero no limitado a,
+
+             <!-- Grid column -->
+             <div class="col-md-6 mt-md-0 mt-3">
+
+                 <!-- Content -->
+
+                 <p class="copyright-text">Copyright &copy; 2019-8127 All Rights Reserved by
+                     <a href="#">Wilmer Terrero & Naomi Céspedes</a>.
+                     <br> Todos los contenidos de este Sitio (Incluyendo, pero no limitado a,
                      texto, logotipos, contenido, fotografías, audio, botones, nombres comerciales y vídeo) están
                      sujetos a derechos de propiedad por las leyes de Derechos de autor.
                  </p>
-             </div>
 
-             <div class="col-xs-6 col-md-3">
-                 <ul class="social-icons">
-                     <br><br><br>
-                     <li><a class="facebook" href="#"><i class="fab fa-facebook"></i></a></li>
-                     <li><a class="twitter" href="#"><i class="fab fa-twitter"></i></a></li>
-                     <li><a class="github" href="#"><i class="fab fa-github"></i></a></li>
-                     <li><a class="youtube" href="#"><i class="fab fa-youtube"></i></a></li>
+             </div>
+             <!-- Grid column -->
+
+             <hr class="clearfix w-100 d-md-none pb-3">
+
+             <!-- Grid column -->
+             <div class="col-md-3 mb-md-0 mb-3">
+
+                 <ul class="list-unstyled">
+                     <ul class="social-icons">
+                         <br><br><br>
+                         <li><a class="facebook" href="#"><i class="fab fa-facebook"></i></a></li>
+                         <li><a class="twitter" href="#"><i class="fab fa-twitter"></i></a></li>
+                         <li><a class="github" href="#"><i class="fab fa-github"></i></a></li>
+                         <li><a class="youtube" href="#"><i class="fab fa-youtube"></i></a></li>
+                     </ul>
                  </ul>
 
              </div>
+             <!-- Grid column -->
 
-             <div class="col-xs-6 col-md-3">
-                 <h6>Mas Informacion</h6>
-                 <ul class="footer-links">
-                     <li><a href="mailto:20198127@itla.edu.do">About Us</a></li>
-                     <li><a href="mailto:20198127@itla.edu.do">Contact Us</a></li>
+             <!-- Grid column -->
+             <div class="col-md-3 mb-md-0 mb-3">
+
+
+                 <ul class="list-unstyled">
+                     <ul class="footer-links">
+                         <li><a href="mailto:20198127@itla.edu.do">About Us</a></li>
+                         <li><a href="mailto:20198127@itla.edu.do">Contact Us</a></li>
+                     </ul>
                  </ul>
+
              </div>
+             <!-- Grid column -->
+
          </div>
-         <hr>
+         <!-- Grid row -->
+
      </div>
+     <!-- Footer Links -->
+
+     <!-- Copyright -->
+     <div class="footer-copyright text-center py-3">© 2020 Copyright:
+         <p> Trailery </P>
+     </div>
+     <!-- Copyright -->
+
  </footer>
+ <!-- Footer -->
